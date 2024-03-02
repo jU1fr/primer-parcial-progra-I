5d47d9440a8f3acdb57ac11c5920f497f8bdbca1
Juan Carlos Manuel Lechuga Ortiz21 <134109649+jU1fr@users.noreply.github.com>
2024-03-02 11:02:37 -0600

    Add files via upload

---
+++ ejercicio #1 cajero/cajero.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+class CuentaBancaria
+{
+    private double saldo;
+
+    public CuentaBancaria(double saldoInicial)
+    {
+        saldo = saldoInicial;
+    }
+
+    public void Depositar(double cantidad)
+    {
+        saldo += cantidad;
+        Console.WriteLine("Depositaste " + cantidad);
+        Console.WriteLine("Nuevo saldo: " + saldo);
+    }
+
+    public void Retirar(double cantidad)
+    {
+        if (cantidad <= saldo)
+        {
+            saldo -= cantidad;
+            Console.WriteLine("Retiraste " + cantidad);
+            Console.WriteLine("Nuevo saldo: " + saldo);
+        }
+        else
+        {
+            Console.WriteLine("Fondos insuficientes");
+        }
+    }
+
+    public double GetSaldo()
+    {
+        return saldo;
+    }
+}
+
+class CajeroAutomatico
+{
+
+    static void Main(string[] args)
+    {
+
+        CuentaBancaria cuenta = new CuentaBancaria(500.0);
+
+        int opcion;
+
+        do
+        {
+            Console.WriteLine("Seleccione una opción:");
+            Console.WriteLine("1. Ver saldo");
+            Console.WriteLine("2. Depositar");
+            Console.WriteLine("3. Retirar");
+            Console.WriteLine("4. Salir");
+
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Su saldo es: " + cuenta.GetSaldo());
+                    break;
+                case 2:
+                    Console.WriteLine("Ingrese monto a depositar:");
+                    double montoDeposito = double.Parse(Console.ReadLine());
+                    cuenta.Depositar(montoDeposito);
+                    break;
+                case 3:
+                    Console.WriteLine("Ingrese monto a retirar:");
+                    double montoRetiro = double.Parse(Console.ReadLine());
+                    cuenta.Retirar(montoRetiro);
+                    break;
+            }
+
+        } while (opcion != 4);
+
+    }
+
+}
---
+++ ejercicio #2 programa/ejercicio #2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        float numero, suma = 0, contador = 0;
+        float promedio;
+
+        Console.Write("Ingresa un número (0 para salir): ");
+        numero = float.Parse(Console.ReadLine());
+
+        while (numero != 0)
+        {
+            suma += numero;
+            contador++;
+
+            Console.Write("Ingresa otro número (0 para salir): ");
+            numero = float.Parse(Console.ReadLine());
+        }
+
+        if (contador > 0)
+        {
+            promedio = suma / contador;
+            Console.WriteLine("La suma de todos los números es: " + suma);
+            Console.WriteLine("El promedio es: " + promedio);
+        }
+        else
+        {
+            Console.WriteLine("No ingresaste números");
+        }
+    }
+}
